04632564f9f31ea96f993d542fb50cec5355187e
Eric <ericfoa@al.insper.edu.br>
2020-06-14 12:54:54 -0300

    Execução de um teste específico

--- src/mintest/runner.h
+++ src/mintest/runner.h
@@ -1,18 +1,49 @@
 
+#include <stdio.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+ 
     int size = sizeof(all_tests)/sizeof(test_data);
-    printf("Running %d tests:\n", size);
-    printf("=====================\n\n");
     int pass_count = 0;
-    for (int i = 0; i < size; i++) {
-        if (all_tests[i].function() >= 0) {
-            printf("%s: [PASS]\n", all_tests[i].name);
-            pass_count++;
-        };
+
+    if (argc == 1) {
+        
+        printf("Running %d tests:\n", size);
+        printf("=====================\n\n");
+    
+        for (int i = 0; i < size; i++) {
+            if (all_tests[i].function() >= 0) {
+                printf("%s: [PASS]\n", all_tests[i].name);
+                pass_count++;
+            };
+        }
+
+        printf("\n\n=====================\n");
+        printf("%d/%d tests passed\n", pass_count, size);
+        return 0;
+    }
+
+    else {
+        printf("Running %d tests:\n", argc-1);
+        printf("=====================\n\n");
+
+        for (int i = 0; i < argc; i++) {
+            
+            if (all_tests[i].function() >= 0) {
+                if (strcmp(all_tests[i].name, argv[i+1]) == 0) {
+                    printf("%s: [PASS]\n", all_tests[i].name);
+                    pass_count++;
+                
+                }
+            }
+        }
+        
+        printf("\n\n=====================\n");
+        printf("%d/%d tests passed\n", pass_count, argc-1);
+        return 0;
     }
     
-    printf("\n\n=====================\n");
-    printf("%d/%d tests passed\n", pass_count, size);
-    return 0;
+    
+    
 }
