090cdd237a197ccdd6f5e3161af1a7e8f63790f2
Eric <ericfoa@al.insper.edu.br>
2020-06-14 15:24:56 -0300

    Execução de um teste específico

--- src/mintest/runner.h
+++ src/mintest/runner.h
@@ -28,16 +28,15 @@ int main(int argc, char *argv[]) {
         printf("Running %d tests:\n", argc-1);
         printf("=====================\n\n");
 
-        for (int i = 0; i < argc; i++) {
-            
-            if (all_tests[i].function() >= 0) {
-                if (strcmp(all_tests[i].name, argv[i+1]) == 0) {
+        for (int i = 0; i < size; i++) {
+            if (strcmp(all_tests[i].name, argv[1]) == 0) {
+                if (all_tests[i].function() >= 0) {
                     printf("%s: [PASS]\n", all_tests[i].name);
                     pass_count++;
-                
                 }
             }
         }
+        
         
         printf("\n\n=====================\n");
         printf("%d/%d tests passed\n", pass_count, argc-1);
